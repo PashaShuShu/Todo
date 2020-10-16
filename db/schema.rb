d1db7fd79dbdeb36230aaaa64d8b0a1d872ebba4
Pasha <honcharenkopavel@gmail.com>
2020-10-16 14:51:08 +0300

    add scss file and now its looks preaty good

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_112814) do
+ActiveRecord::Schema.define(version: 2020_10_16_114036) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
     t.integer "todo_list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
