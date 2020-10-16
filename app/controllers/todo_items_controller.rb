9595376f1632959b4196195b0ad979d767b6ef85
Pasha <honcharenkopavel@gmail.com>
2020-10-16 14:38:30 +0300

    ability to delete todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,6 +4,15 @@ class TodoItemsController < ApplicationController
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
    end
+   def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+     flash[:success] = "Todo List item was deleted."
+    else
+     flash[:error] = "Todo List item could not be deleted."
+    end
+    redirect_to @todo_list 
+   end
    private
    def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
